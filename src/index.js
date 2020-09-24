7f1278deab971d4e45e66b7c066b11061fe7d0ae
dekk <dekkservidores@gmail.com>
2020-09-24 12:18:14 -0500

    Cambio de vista

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ require("./js/app")
 
 import "./scss/main.scss"
 import './scss/fonts.scss'
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+import { Home } from "./js/app";
+
+document.addEventListener("DOMContentLoaded", () => {
+    let home = new Home()
+})
---
+++ src/js/app.js
@@ -0,0 +1,70 @@
+export class Home {
+    constructor() {
+        this.scrollMovement();
+        this.activeNav()
+    }
+    scrollMovement() {
+        this.moveScroll("down", "aboutme")
+
+        this.moveScroll("btnaboutmeportada", "aboutme")
+
+        this.moveScroll("homebtn", "portada")
+        this.moveScroll("aboutbtn", "aboutme")
+        this.moveScroll("resumenbtn", "resume")
+        this.moveScroll("myworkbtn", "mywork")
+    }
+
+    moveScroll(scroll, section) {
+        let btn = document.getElementById(`${scroll}`)
+        let sizeSection = document.getElementById(`${section}`).offsetTop
+        if( btn ) {
+            btn.addEventListener("click", () => {
+                window.scrollTo({
+                    top: sizeSection,
+                    behavior: 'smooth'
+                })
+            })
+        }
+    }
+
+    activeNav() {
+        const portada   = document.getElementById("portada").offsetTop
+        const aboutme   = document.getElementById("aboutme").offsetTop
+        const resume    = document.getElementById("resume").offsetTop
+        const mywork    = document.getElementById("mywork").offsetTop
+
+        const homebtn = document.getElementById("homebtn")
+        const aboutbtn = document.getElementById("aboutbtn")
+        const resumenbtn = document.getElementById("resumenbtn")
+        const myworkbtn = document.getElementById("myworkbtn")
+
+        console.log(mywork)
+
+        window.addEventListener("scroll", () => {
+            let scroll = window.scrollY
+            if( scroll ==  portada ) {
+                homebtn.classList.add("active")
+                aboutbtn.classList.remove("active")
+                resumenbtn.classList.remove("active")
+                myworkbtn.classList.remove("active")
+            } else if( scroll == aboutme ) {
+                aboutbtn.classList.add("active")
+                homebtn.classList.remove("active")
+                resumenbtn.classList.remove("active")
+                myworkbtn.classList.remove("active")
+
+            } else if ( scroll == resume ) {
+                homebtn.classList.remove("active")
+                aboutbtn.classList.remove("active")
+                resumenbtn.classList.add("active")
+                myworkbtn.classList.remove("active")
+
+            } else if( scroll == mywork ) {
+                homebtn.classList.remove("active")
+                aboutbtn.classList.remove("active")
+                resumenbtn.classList.remove("active")
+                myworkbtn.classList.add("active")
+            }
+        })
+    }
+}
