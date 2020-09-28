4c6582104e1977d78da33b208576a9de02aeb9a8
dekk <dekkservidores@gmail.com>
2020-09-28 15:00:44 -0500

    Responsive en progreso

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import { Home } from "./js/app";
 import { Modal } from "./js/modal"
 import { PortadaAnimation } from './js/PortadaAnimation';
+import { Header } from "./js/header";
 
 document.addEventListener("DOMContentLoaded", () => {
     let home    = new Home();
     let modal   = new Modal();
     let portada = new PortadaAnimation();
+    let header  = new Header();
 })
---
+++ src/js/header.js
@@ -0,0 +1,26 @@
+export class Header {
+    constructor() {
+        this.bars = document.getElementById("bars")
+
+        this.openAndClose()
+        this.closeMenuItem()
+    }
+    openAndClose() {
+        if( this.bars ) {
+            this.bars.addEventListener("click", () => {
+                const navbar = document.querySelector(".navbar")
+                navbar.classList.toggle("active")
+            })
+        }
+    }
+    closeMenuItem() {
+        const navbar = document.querySelectorAll(".navbar .lista li")
+        for( let i =0; i < navbar.length; i++ ) {
+            let item = navbar[i]
+            item.addEventListener("click", () => {
+                console.log(item)
+                this.bars.click()
+            })
+        }
+    }
+}
